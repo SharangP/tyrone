1265bf561e5114fb2dcc693e1550f5bf0cb4f37c
SharangP <sharang.phadke@gmail.com>
2013-01-10 23:14:44 -0500

    streaming and stuff

--- bitly_test.py
+++ bitly_test.py
@@ -2,8 +2,23 @@ import bitly_api
 import time
 
 C = bitly_api.Connection(access_token='61c2b3d767d10e5a58b62d0c7ee1bfa856ac69f8')
+Links = {}
+# Categories = db.getCategories() # get the category mapping table
 
-hotphrases = C.highvalue(limit=10000)
-print 'BEGIN NEW SET OF PHRASES'
-for link in hotphrases['values']:
-  print C.clicks_by_day(shortUrl=link)[0]['clicks'], C.info(link=link)[0]['title']
+linkId = 1
+# linkId = db.maxId()# get the max id from the db if running this again
+
+while True:
+    hot = C.highvalue(limit=100)
+    for link in hot['values']:
+        key = link.split('bit.ly/')[1]
+        if not Links.has_key(key):
+            Links[key] = linkId
+            print "new link!", key, Links[key]
+            # insert (linkId,key) into LINKS table
+            for category in C.link_category(link):
+                print '\t',category
+                # insert (linkId,catID) into CATEGORIES table
+            linkId += 1
+#        print C.clicks_by_day(shortUrl=link)[0]['clicks'], C.info(link=link)[0]['title']
+    time.sleep(10) #repeat every 30 mins
