222bd359455710897df63f568b71250c678e331c
SharangP <sharang.phadke@gmail.com>
2013-01-12 01:53:36 -0500

    after first long run. ignore .db files

--- highMinuteClicks.py
+++ highMinuteClicks.py
@@ -5,7 +5,7 @@ from db import Database
 C = bitly_api.Connection(access_token='20dbd7d26d6f4d7c9c6d70b22236797b48a7b7af')
 Links = {}
 Clicks = {}
-DB = Database('bitly1')
+DB = Database('bitly1.db')
 
 while True:
     hot = C.highvalue(limit=300)
